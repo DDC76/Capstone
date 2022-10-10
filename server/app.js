38d3c515a6f7eea88aa8e164d32385e9e550681e
Deionne <111102092+DDC76@users.noreply.github.com>
2022-10-10 14:52:03 -0500

    mongo

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
-
+const mongoose = require("mongoose");
 
 
 
 dotenv.config();
 
-
-
 // Initialize the http server
 const app = express();
 
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
 
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
 
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
