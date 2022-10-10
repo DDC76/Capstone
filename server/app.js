ccc9b50c5515f7d039c9dc0595adfda7db7349b6
Deionne <111102092+DDC76@users.noreply.github.com>
2022-10-10 15:13:35 -0500

    mongofix

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-
-
 dotenv.config();
 
 // Initialize the http server
@@ -27,10 +25,7 @@ const logging = (request, response, next) => {
 app.use(express.json());
 app.use(logging);
 
-
-// Initialize the http server
-const server = http.createServer((request, response) => {
-   // Handle the request with HTTP GET method from http://localhost:4040/status
+// Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
   // Create the headers for response by default 200
   // Create the response body
@@ -45,7 +40,6 @@ app.get("/echo/:input", (request, response) => {
 
 const PORT = process.env.API_PORT || 4040;
 // we use || to provide a default value
-
 
 // Let the humans know I am running and listening on 4040
 app.listen(4040, () => console.log(`Listening on port ${PORT}`));
