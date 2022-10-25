99135cc61fd9db9d523225c4f31f9c0b3ec06109
Deionne <111102092+DDC76@users.noreply.github.com>
2022-10-25 13:25:23 -0500

    mapupdates

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import html from "html-literal";
 
 export default links => html`
   <nav>
-    <i class="fas fa-bars"></i>
+    <i class="fas fa-bars"><span class="fa-li"></span></i>
     <ul class="hidden--mobile nav-links">
       ${links
         .map(
